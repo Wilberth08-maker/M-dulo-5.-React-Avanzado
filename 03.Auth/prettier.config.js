3551f63a9340f98393542289943658ab475c37ff
Wilberth Adair Martínez Puc <wilberthmtzpuc@gmail.com>
2025-07-15 21:15:37 -0500

    🎉 Configuración de prettier

---
+++ 03.Auth/prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // Preferir comillas dobles
+    singleQuote: false, // false: comillas dobles, true: comillas simples
+    jsxSingleQuote: false, // false: comillas dobles, true: comillas simples
+    trailingComma: 'es5', // Agregar coma al final de las lineas de objetos/arrays(estandar moderno)
+    semi: true, // Agregar punto y coma al final de las lineas
+    arrowParens: 'always', // Agregar parentesis al final de las funciones de flecha, mejora la legibilidad
+}

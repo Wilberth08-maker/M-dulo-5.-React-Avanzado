88f138b68b760afc005b4179a98839ee4013a46c
Wilberth Adair Martínez Puc <wilberthmtzpuc@gmail.com>
2025-07-17 17:57:47 -0500

    🎉 Rutas públicas

--- 03.Auth/src/routes/RoutesIndex.jsx
+++ 03.Auth/src/routes/RoutesIndex.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from '@/pages/Home'
+import Dashboard from '@/pages/Dashboard'
+import Login from '@/pages/Login'
+import Secret from '@/pages/Secret'
+import Signup from '@/pages/Signup'
 
 const RoutesIndex = () => {
     return (
-        <div>
-
-        </div>
+        <>
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/secret' element={<Secret />} />
+            <Route path='/signup' element={<Signup />} />
+        </Routes>
+        </>
     )
 }
 

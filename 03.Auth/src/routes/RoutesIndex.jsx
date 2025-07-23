5e0aff616e236e3703099e1b6ff61cb2e6da3733
Wilberth Adair Martínez Puc <wilberthmtzpuc@gmail.com>
2025-07-23 16:59:36 -0500

    🐛 Fix bug

--- 03.Auth/src/routes/RoutesIndex.jsx
+++ 03.Auth/src/routes/RoutesIndex.jsx
@@ -1,34 +1,34 @@
-import {Routes, Route} from 'react-router-dom'
-import Home from '@/pages/Home/Home'
-import Dashboard from '@/pages/Dashboard/Dashboard'
-import Login from '@/pages/Login/Login'
-import Secret from '@/pages/Secret/Secret'
-import Signup from '@/pages/Signup/Signup'
-import ProtectedRoute from './ProtectedRoute'
+import { Routes, Route } from "react-router-dom"
+import Home from "@/pages/Home/Home"
+import Dashboard from "@/pages/Dashboard/Dashboard"
+import Login from "@/pages/Login/Login"
+import Secret from "@/pages/Secret/Secret"
+import Signup from "@/pages/Signup/Signup"
+import ProtectedRoute from "./ProtectedRoute"
 
 const RoutesIndex = () => {
     return (
-        <>
+    <>
         <Routes>
-            <Route path='/' element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route 
-            path='/dashboard' 
+            path="/dashboard" 
             element={
-                <ProtectedRoute>
-                    <Dashboard />
-                </ProtectedRoute>
+            <ProtectedRoute>
+                <Dashboard />
+            </ProtectedRoute>
             } />
             <Route 
-            path='/secret' 
+            path="/secret" 
             element={
-                <ProtectedRoute>
-                    <Secret />
-                </ProtectedRoute>
+            <ProtectedRoute>
+                <Secret />
+            </ProtectedRoute>
             } />
-            <Route path='/login' element={<Login />} />
-            <Route path='/signup' element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
         </Routes>
-        </>
+    </>
     )
 }
 

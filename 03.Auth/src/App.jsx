949a6dae2e3ba5c1dc91f4952f6b26cd73fe51be
Wilberth Adair Martínez Puc <wilberthmtzpuc@gmail.com>
2025-07-23 16:07:32 -0500

    🎉 Envolvemos la app con el contexto

--- 03.Auth/src/App.jsx
+++ 03.Auth/src/App.jsx
@@ -2,14 +2,17 @@ import Header from '@/components/Header'
 import RoutesIndex from '@/routes/RoutesIndex'
 import { ToastContainer } from 'react-toastify' // Importamos el ToastContainer
 import 'react-toastify/dist/ReactToastify.css' // Estilos por defecto de react-toastify
+import { AuthProvider } from '@/context/AuthContext'
 
 function App() {
   
   return (
     <>
       <ToastContainer />
+      <AuthProvider>
       <Header />
       <RoutesIndex />
+      </AuthProvider>
     </>
   )
 }
